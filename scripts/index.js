843938388bb4d3b5182eaae3668ae9cad3565b1f
Anton Böhler <antboehler@gmail.com>
2022-11-03 00:15:47 +0100

    fix mobile layouting

--- scripts/index.js
+++ scripts/index.js
@@ -5,6 +5,8 @@ window.onload = async function(){
         navigator.serviceWorker.register("./serviceWorker.js");
     }
 
+    setViewportSize();
+
     try{
         quiz = await (await fetch("quiz.txt")).text();
         localStorage.setItem("quizQuestions", quiz);
@@ -16,4 +18,23 @@ window.onload = async function(){
     loadQuestionList();
     loadRandomQuestion();
     loadStatistic();
+}
+
+window.onresize = () => {
+    if(document.activeElement.tagName !== 'TEXTAREA' && document.activeElement.tagName !== 'INPUT'){
+        setViewportSize();
+    }
+}
+
+window.onorientationchange = () => {
+    // check if keyboard is used
+    if(document.activeElement.tagName === 'TEXTAREA' || document.activeElement.tagName === 'INPUT'){
+        document.activeElement.blur();
+    }
+    setViewportSize();
+}
+
+function setViewportSize(){
+    document.documentElement.style.setProperty('--screen-width', `${window.innerWidth}px`);
+    document.documentElement.style.setProperty('--screen-height', `${window.innerHeight}px`);
 }
