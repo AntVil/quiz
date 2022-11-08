19ca10d7cc6d2664732edceb02eebba1e71525ca
AntVil <antboehler@gmail.com>
2022-11-08 16:10:16 +0100

    add dynamic statistic

--- scripts/index.js
+++ scripts/index.js
@@ -7,13 +7,7 @@ window.onload = async function(){
 
     setViewportSize();
 
-    try{
-        quiz = await (await fetch("quiz.txt")).text();
-        localStorage.setItem("quizQuestions", quiz);
-    }catch{
-        quiz = localStorage.getItem("quizQuestions");
-    }
-    quiz = quiz.split("\n").map(s => s.trim()).filter(s => s.length > 0);
+    await loadQuiz();
 
     loadSettings();
     
@@ -39,4 +33,42 @@ window.onorientationchange = () => {
 function setViewportSize(){
     document.documentElement.style.setProperty('--screen-width', `${window.innerWidth}px`);
     document.documentElement.style.setProperty('--screen-height', `${window.innerHeight}px`);
+}
+
+async function loadQuiz(){
+    let downloadedQuiz;
+    
+    try{
+        downloadedQuiz = (await (await fetch("quiz.txt")).text()).split("\n").map(s => s.trim()).filter(s => s.length > 0);
+    }catch{
+
+    }
+    
+    let cachedQuiz = localStorage.getItem("quizQuestions").split("\n").map(s => s.trim()).filter(s => s.length > 0);
+
+    if(downloadedQuiz === undefined){
+        quiz = cachedQuiz;
+    }else{
+        quiz = downloadedQuiz;
+        
+        let cachedStatistic = localStorage.getItem("quizStatistics");
+        let newStatistic = "";
+
+        for(let i=0;i<quiz.length;i++){
+            let index = -1;
+            for(let j=0;j<cachedQuiz.length;j++){
+                if(quiz[i].startsWith(`${cachedQuiz[j].split("#")[0]}#`)){
+                    index = j;
+                    break;
+                }
+            }
+            if(index !== -1 && cachedStatistic[index] !== undefined){
+                newStatistic += cachedStatistic[index];
+            }else{
+                newStatistic += STATISTICS_MAX_COUNT;
+            }
+        }
+
+        localStorage.setItem("quizStatistics", newStatistic);
+    }
 }
